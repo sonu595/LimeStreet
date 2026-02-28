bf0f80de5d788b2c834f92c961411866ef4e562f
Sonu Singh <shadow.pentest1899@gmail.com>
2026-02-28 15:00:14 +0530

    change package name

--- e-commerce/src/main/java/com/Startup/Ecommerce/LimeStreetApplication.java
+++ e-commerce/src/main/java/com/Startup/Ecommerce/LimeStreetApplication.java
@@ -1,4 +1,4 @@
-package com.Startup.Ecomers;
+package com.Startup.Ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
