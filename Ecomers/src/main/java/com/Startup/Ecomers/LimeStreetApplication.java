0ee569944b03cb18f62033531d77043cfb663b7f
Sonu Singh <shadow.pentest1899@gmail.com>
2026-02-28 14:53:39 +0530

    Initial project uploade

---
+++ Ecomers/src/main/java/com/Startup/Ecomers/LimeStreetApplication.java
@@ -0,0 +1,13 @@
+package com.Startup.Ecomers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimeStreetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimeStreetApplication.class, args);
+	}
+
+}
---
+++ Ecomers/src/test/java/com/Startup/Ecomers/LimeStreetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Startup.Ecomers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimeStreetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
